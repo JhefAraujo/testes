36268e98b9f75e5d2cda90b637f9ea7a80bb3da0
Jhefferson Araujo <jhefferson.araujo@gmail.com>
2023-10-30 16:39:04 -0300

    onscroll changes

--- main.js
+++ main.js
@@ -3,5 +3,25 @@ function changeState(id) {
     setTimeout(() => {
         document.getElementById(id).style.display = 'none';
         document.getElementById('scroller').style.display = 'flex';
+        document.getElementById('myHeader').style.display = 'flex';
     }, 1000);
 }
+
+function changeBack(id) {
+    document.getElementById(id).style.animation = 'sumir 1s';
+    setTimeout(() => {
+        document.getElementById(id).style.display = 'none';
+        document.getElementById('main').style.display = 'flex';
+        document.getElementById('myHeader').style.display = 'none';
+    }, 1000);
+}
+
+main.addEventListener('wheel', () => {
+    changeState('main')
+})
+
+window.addEventListener('scroll', (scroll) => {
+    if (scroll > 0) {
+        console.log('cima')
+    }
+})
